151ec76afcd0e9c24c9cd0d7bcb9d8078e1ce461
ekemabasi <ekemobong@gmail.com>
2022-12-08 11:02:44 -0800

    C is fun

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,20 +1,23 @@
-#!/bin/bash
 #include <stdio.h>
+
+
 
 /**
  *
- * * main - main block
+ *  *main - Entry point
  *
- * * Return: 0
+ *   *Return: Always 0 (Success)
  *
- * */
+ *    */
+
+
 
 int main(void)
 
 {
 
-	puts("\"Programming is like building a multilingual puzzle");
+		puts("\"Programming is like building a multilingual puzzle");
 
-	return (0);
+			return (0);
 
 }
