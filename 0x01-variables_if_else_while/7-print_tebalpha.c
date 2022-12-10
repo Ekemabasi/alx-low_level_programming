22daa4c2ba5b6e93ccc3e5f559ffe2933ce3589c
ekemabasi <ekemobong@gmail.com>
2022-12-10 02:31:20 -0800

    Printing from z to a

---
+++ 0x01-variables_if_else_while/7-print_tebalpha.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - printingg from z - a
+ * Return: Alway 0 (Success)
+ */
+int main(void)
+{
+char n = 'z';
+while (n >=  'a')
+{
+putchar(n);
+n--;
+}
+putchar('\n');
+
+return (0);
+}
