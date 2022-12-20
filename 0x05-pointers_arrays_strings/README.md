24d9cb3a6e7b9dc2ff74197138457f265d940cf7
ekemabasi <ekemobong@gmail.com>
2022-12-20 09:56:09 -0800

    Array

---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,6 @@
+This programme does the demonstrate;
+What pointers are and how they are used
+What arrays and how they are used
+The differences between pointers and arrays
+How to use strings and how to manipulate them
+Scope of variables
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+
+/**
+ * void prototypes()
+ * char prototypes()
+ * int prototypes()
+ */
+
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+void _puts(char *str);
+void print_rev(char *s);
+int _putchar(char c);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n)
+#endif /*MAIN_H*/
